aba36262fc05069f83c9e8dcda67f45d24e6f6b9
Yelyzaveta Lykhomanova <44315985+LisaBugaichuk@users.noreply.github.com>
2023-09-27 11:10:37 +0300

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class Main {
+
+	public static void main(String[] args) {
+		;
+	}
+
+}
