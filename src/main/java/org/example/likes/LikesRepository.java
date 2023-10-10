c286e1196860c811cc46e75a651589866bd1b20e
Yelyzaveta Lykhomanova <bug.liza@gmail.com>
2023-10-10 13:44:00 +0300

    Commit 1 : create likes table

--- src/main/java/org/example/likes/LikesRepository.java
+++ src/main/java/org/example/likes/LikesRepository.java
@@ -1,8 +1,8 @@
-package org.example.frienships;
+package org.example.likes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FriendshipsRepository extends JpaRepository<FriendshipsEntity, Long> {
+public interface LikesRepository extends JpaRepository <LikeEntity, Long> {
 }
