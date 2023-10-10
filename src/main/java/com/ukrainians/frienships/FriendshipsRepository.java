8b002ff9be330c3d307adf6051e5ad65713255bd
Yelyzaveta Lykhomanova <bug.liza@gmail.com>
2023-10-10 15:30:37 +0300

    Corrected the mistakes

---
+++ src/main/java/com/ukrainians/frienships/FriendshipsRepository.java
@@ -0,0 +1,8 @@
+package com.ukrainians.frienships;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FriendshipsRepository extends JpaRepository<FriendshipsEntity, Long> {
+}
