41ca7c1e94cb674bb5c0ce34d3d380f395d56348
Antonio <oz0969991415@gmail.com>
2023-10-22 20:52:20 +0300

    fixed gradlew

--- src/main/java/com/ukrainians/entity/Post.java
+++ src/main/java/com/ukrainians/entity/Post.java
@@ -1,2 +1,26 @@
-package com.ukrainians.entity;public class Post {
+package com.ukrainians.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name ="POST")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "CONTENT", nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "TIMESTAMP")
+    private Date timestamp;
 }
--- src/main/java/com/ukrainians/repository/PostRepository.java
+++ src/main/java/com/ukrainians/repository/PostRepository.java
@@ -1,2 +1,7 @@
-package com.ukrainians.repository;public interface PostRepository {
+package com.ukrainians.repository;
+
+import com.ukrainians.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
