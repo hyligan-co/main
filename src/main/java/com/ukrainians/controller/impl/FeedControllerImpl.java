6c369f58ac270a284e92334d5d416a5cc5d109ed
Knotty123230 <122396662+Knotty123230@users.noreply.github.com>
2023-11-02 19:20:13 +0200

    fix

--- src/main/java/com/ukrainians/controller/impl/FeedControllerImpl.java
+++ src/main/java/com/ukrainians/controller/impl/FeedControllerImpl.java
@@ -6,6 +6,7 @@ import com.ukrainians.services.PostService;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -19,13 +20,13 @@ public class FeedControllerImpl implements FeedController {
 
     @Override
     public @ResponseBody List<PostResponse> getFeeds(Long id) {
-        List<PostResponse> postResponses = new java.util.ArrayList<>(postService.findAllPostsById(id))
+        List<PostResponse> postResponses = new ArrayList<>(new ArrayList<>(postService.findAllPostsById(id))
                 .stream()
                 .map(post -> new PostResponse(post.getId(),
                         post.getUser().getId(),
                         post.getContent(),
                         post.getCreatedAt()))
-                .toList();
+                .toList());
         postResponses.sort(Comparator.comparing(PostResponse::getTimestamp));
         return postResponses;
     }
